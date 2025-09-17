aded8f3ad291541867af061b7e6513ed24736d6a
Mingzhe994 <tuzuki@126.com>
2025-09-17 23:57:35 +0800

    添加routes初始化文件

---
+++ routes/__init__.py
@@ -0,0 +1,7 @@
+from routes.main_routes import main_bp
+from routes.task_routes import task_bp
+from routes.workflow_routes import workflow_bp
+from routes.issue_routes import issue_bp
+from routes.analytics_routes import analytics_bp
+
+__all__ = ['main_bp', 'task_bp', 'workflow_bp', 'issue_bp', 'analytics_bp']
